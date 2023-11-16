28f0ca2db0b5661895d99d1a01af054b3233be63
joexy <josephethethowo@gmail.com>
2023-11-16 09:17:08 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Barchart from './Chart/Barchart'
+import './App.css'
+import Linechart from './Chart/Linechart'
+import Doughnutchart from './Chart/Doughnut'
+
+function App() {
+ 
+
+  return (
+    <>
+      <Barchart />
+      <Doughnutchart />
+      <Linechart />
+    </>
+  )
+}
+
+export default App
---
+++ src/Chart/Linechart.jsx
@@ -0,0 +1,59 @@
+import {Chart as ChartJS, LineElement, PointElement, CategoryScale, LinearScale} from 'chart.js';
+import {Line} from 'react-chartjs-2';
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement);
+
+const Linechart = () => {
+    var data = {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options = {
+        maintainAspectRatio: false,
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        },
+        legend:{
+            labels:{
+                fontSize: 26
+            }
+        }
+    };
+  return (
+    <div>
+        <Line
+        data={data}
+        height={400}
+        options={options}
+        />
+    </div>
+  )
+}
+
+export default Linechart
